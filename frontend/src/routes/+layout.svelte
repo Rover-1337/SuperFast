e30734358a611365b825b17bc08d170986113836
Rover <82412232+Rover-1337@users.noreply.github.com>
2024-08-21 23:39:51 +0200

    push

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,6 +2,20 @@
 	import Header from '$lib/components/Header.svelte';
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		// @ts-ignore
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			// @ts-ignore
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <svelte:head>
