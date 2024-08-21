300314996403b2183bc65ec2ed003ad283c068e9
Rover <82412232+Rover-1337@users.noreply.github.com>
2024-08-21 23:51:39 +0200

    push

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,33 +2,50 @@
 	import { API_URL } from '$lib/constant';
 	import * as Card from '$lib/components/ui/card';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import { Skeleton } from '$lib/components/ui/skeleton';
+
+	let rerender = 0;
 
 	async function fetchPosts() {
 		const res = await fetch(`${API_URL}/api/blogPosts`);
 		const data = await res.json();
 		return data;
 	}
+
+	async function deletepost(id: string) {
+		const res = await fetch(`${API_URL}/api/deleteBlogPost/${id}`);
+
+		if (res.ok) {
+			rerender++;
+		}
+	}
 </script>
 
-{#await fetchPosts() then posts}
-	{#if posts.data == null}
-		<p>No posts found</p>
-	{:else}{#each posts.data as post}
-			<div>
-				<Card.Root class="mx-auto my-4 w-1/3">
-					<Card.Header>
-						<Card.Title>
-							<a href={`/blog/${post.id}`}>{post.title}</a>
-						</Card.Title>
-					</Card.Header>
-					<Card.Content>
-						<Button href={`/blog/${post.id}`}>Read more</Button>
-					</Card.Content>
-				</Card.Root>
-			</div>
-		{/each}
-	{/if}
-{:catch error}
-	<p>{error.message}</p>
-{/await}
+{#key rerender}
+	{#await fetchPosts() then posts}
+		{#if posts.data == null}
+			<p>No posts found</p>
+		{:else}{#each posts.data as post}
+				<div>
+					<Card.Root class="mx-auto my-4 w-1/3">
+						<Card.Header>
+							<Card.Title>
+								<a href={`/blog/${post.id}`}>{post.title}</a>
+							</Card.Title>
+						</Card.Header>
+						<Card.Content class="flex justify-between">
+							<Button href={`/blog/${post.id}`}>Read more</Button>
+
+							<Button
+								on:click={() => deletepost(post.id)}
+								href={`/api/deleteBlogPost/${post.id}`}
+								variant="destructive">Delete</Button
+							>
+						</Card.Content>
+					</Card.Root>
+				</div>
+			{/each}
+		{/if}
+	{:catch error}
+		<p>{error.message}</p>
+	{/await}
+{/key}
