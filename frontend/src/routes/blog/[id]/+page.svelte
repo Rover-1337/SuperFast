7f2cc2ed325e8895bfee489f8abc0a46237c0602
Rover <82412232+Rover-1337@users.noreply.github.com>
2024-08-22 13:29:04 +0200

    push

--- frontend/src/routes/blog/[id]/+page.svelte
+++ frontend/src/routes/blog/[id]/+page.svelte
@@ -11,21 +11,23 @@
 	}
 </script>
 
-{#await load() then post}
-	<Card.Root class="mx-auto mt-10 w-3/4">
-		<Card.Header>
-			<Card.Title>{post.data.title}</Card.Title>
-			<Card.Description>{post.data.description}</Card.Description>
-		</Card.Header>
-		<Card.Content>
-			<p>{post.data.content}</p>
+{#key $page.params.id}
+	{#await load() then post}
+		<Card.Root class="mx-auto mt-10 w-3/4">
+			<Card.Header>
+				<Card.Title>{post.data.title}</Card.Title>
+				<Card.Description>{post.data.description}</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<p>{post.data.content}</p>
 
-			<Button class="my-4" href="/">Back to blog</Button>
-		</Card.Content>
-		<Card.Footer>
-			<p>{post.data.created_at.split('.')[0]}</p>
-		</Card.Footer>
-	</Card.Root>
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
+				<Button class="my-4" href="/">Back to blog</Button>
+			</Card.Content>
+			<Card.Footer>
+				<p>{post.data.created_at.split('.')[0]}</p>
+			</Card.Footer>
+		</Card.Root>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+{/key}
